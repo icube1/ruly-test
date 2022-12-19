2e8bb1d2ea730bb5edd4f60be26316851ee740ac
Alex <kom.alx.frlv@gmail.com>
2022-12-19 11:00:48 +0500

    hui

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ import fetch from "node-fetch";
 import AdmZip from "adm-zip"
 import * as convert from "xml-js"
 
-
 async function downloadBic(fileUrl, destPath) {
     let data = [];
 
     if (!fileUrl) return Promise.reject(new Error('Invalid fileUrl'));
     if (!destPath) return Promise.reject(new Error('Invalid destPath'));
-
+    console.log('1');
     await new Promise(function (resolve, reject) {
         fetch(fileUrl).then(function (res) {
             const fileStream = fs.createWriteStream(destPath);
             res.body.on('error', reject);
             fileStream.on('finish', resolve);
             res.body.pipe(fileStream);
+	    console.log('2');
         });
     }).then(async () => {
         const zip = AdmZip(destPath);
@@ -24,7 +24,7 @@ async function downloadBic(fileUrl, destPath) {
         let content = zip.readAsText(zipItems[0].entryName);
 
         let items = await convert.xml2js(content).elements[0].elements;
-
+	console.log('3');
         items.forEach((element) => {
 
             // Проверка есть ли Account в элементе банка
@@ -54,6 +54,7 @@ async function downloadBic(fileUrl, destPath) {
 
 let path = './download/bik.zip';
 let fileUrl = 'https://www.cbr.ru/s/newbik'
+let data = await downloadBic(fileUrl, path);
 
-console.log(await downloadBic(fileUrl, path));
+console.log(data);
 
